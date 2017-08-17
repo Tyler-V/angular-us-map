c8622a26af8dd0f7a2f9e29a6a524f8a2cdc85ff
Tyler Vorpahl <tvorpahl@transfinder.com>
2017-08-17 11:38:33 -0400

    Mouse events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { ToastrModule } from 'ngx-toastr';
+
+export var toastrOptions = {  
+    positionClass: 'toast-top-right',
+    timeOut: 2000,
+    closeButton: true,
+    progressBar: true,
+    tapToDismiss: true
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +20,9 @@ import { MapComponent } from './map/map.component';
     MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(toastrOptions), 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, Renderer2 } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, Output, HostBinding, Renderer2 } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { MapsSVG } from './maps';
 
 declare var Raphael;
@@ -8,13 +9,17 @@ declare var Raphael;
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, OnDestroy {
 
   @Input() width: number = 950;
   @Input() height: number = 600;
 
+  @HostBinding('style.width.px') _width = this.width;
+  @HostBinding('style.height') _height = 'initial';
+
   private paper: any;
   private states: Map<any, any> = new Map<any, any>();
+  private listeners: Array<Function> = [];
 
   private options = {
     "fill": "#d3d3d3",
@@ -26,33 +31,59 @@ export class MapComponent implements OnInit {
     "stroke-dasharray": "none"
   }
 
-  constructor(private renderer2: Renderer2) { }
+  constructor(private renderer2: Renderer2, private toastr: ToastrService) {}
 
   ngOnInit() {
-    this.paper = Raphael("container", this.width, this.height), {
-      attr: this.options
-    }
+    this.paper = Raphael("map"), { attr: this.options };
+    this.paper.setViewBox(0, 0, this.width, this.height, true);
+    var svg = document.querySelector("svg");
+    svg.removeAttribute("width");
+    svg.removeAttribute("height");
+
+    this.paper.text(this.width / 2, 15, "Select a state!").attr("font", "30px Caveat Brush").attr("fill", "black");
+
     for (let state in MapsSVG.USA) {
       let path = this.paper.path(MapsSVG.USA[state]).attr(this.options);
       this.states.set(state, path);
     }
+
     this.states.forEach((state, key) => {
       state.color = Raphael.getColor();
+      state.name = key;
       state[0].style.cursor = "pointer";
-      this.renderer2.listen(state[0], "mouseover", () => {
-        state.animate({
-          fill: state.color
-        }, 500);
-        state.toFront();
-        this.paper.safari();
-      });
-      this.renderer2.listen(state[0], "mouseout", () => {
-        state.animate({
-          fill: "#d3d3d3"
-        }, 500);
-        state.toFront();
-        this.paper.safari();
-      });
+      this.listeners.push(this.renderer2.listen(state[0], "mouseover", (e) => {
+        this.onMouseOver(e, state)
+      }));
+      this.listeners.push(this.renderer2.listen(state[0], "mouseout", (e) => {
+        this.onMouseOut(e, state)
+      }));
+      this.listeners.push(this.renderer2.listen(state[0], "click", (e) => {
+        this.onClick(e, state);
+      }));
     });
+
+  }
+
+  ngOnDestroy() {
+    for (let listener of this.listeners)
+      listener();
+  }
+
+  onMouseOver(e: any, state: any) {
+    state.animate({
+      fill: state.color
+    }, 500);
+    state.toFront();
+  }
+
+  onMouseOut(e: any, state: any) {
+    state.animate({
+      fill: "#d3d3d3"
+    }, 500);
+    state.toFront();
+  }
+
+  onClick(e: any, state: any) {    
+    this.toastr.info("Hello from " + state.name + "!");
   }
 }
